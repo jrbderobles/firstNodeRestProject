35b7338926f57ac03c422f1a132d06b2b6b6ecd1
John de Robles <jrbderobles@gmail.com>
2022-09-23 06:55:32 +0800

    Added a signup endpoint using graphql

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const mongoose = require('mongoose');
+const { graphqlHTTP } = require('express-graphql');
 
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
 
 const MONGODB_URI = require('./config').MONGODB_URI;
 
@@ -47,12 +47,32 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+
   next();
 });
 
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
-app.use('/user', userRoutes);
+app.use('/graphql', graphqlHTTP({
+  schema: graphqlSchema,
+  rootValue: graphqlResolver,
+  graphiql: true,
+  formatError(err) {
+    if (!err.originalError) {
+      return err;
+    }
+    const data = err.originalError.data;
+    const code = err.originalError.code || 500;
+    const message = err.message || 'An error occurred.';
+    return {
+      message: message,
+      status: code,
+      data: data
+    }
+  }
+}));
 
 app.use((error, req, res, next) => {
   console.log(error);
@@ -72,11 +92,6 @@ mongoose
   .connect(MONGODB_URI)
   .then(() => {
     console.log('Connected to DB!');
-    const server = app.listen(8080);
-    const io = require('./socket').init(server);
-
-    io.on('connection', socket => {
-      console.log('Client connected');
-    });
+    app.listen(8080);
   })
   .catch(err => console.log(err));
