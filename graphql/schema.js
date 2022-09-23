0add48b0438949b0b7859a460bd24ad7f3a71d37
John de Robles <jrbderobles@gmail.com>
2022-09-23 08:06:38 +0800

    Added getting posts using graphql

--- graphql/schema.js
+++ graphql/schema.js
@@ -37,8 +37,14 @@ module.exports = buildSchema(`
     userId: String!
   }
 
+  type PostData {
+    posts: [Post!]!
+    totalPosts: Int!
+  }
+
   type RootQuery {
     login(email: String!, password: String!): AuthData!
+    posts(page: Int): PostData!
   }
 
   type RootMutation {
