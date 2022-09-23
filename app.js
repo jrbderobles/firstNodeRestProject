a7ead36033085a013e4642fcf14058b41daeceaa
John de Robles <jrbderobles@gmail.com>
2022-09-23 08:19:54 +0800

    Added image upload using graphql

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
@@ -59,6 +60,27 @@ app.use((req, res, next) => {
 
 app.use(auth);
 
+app.put('/post-image', (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error('Not authenticated!');
+  }
+
+  if (!req.file) {
+    return res.status(200).json({
+      message: 'No file provided!'
+    });
+  }
+
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+
+  return res.status(201).json({
+    message: 'File stored.',
+    filePath: req.file.path
+  });
+});
+
 app.use('/graphql', graphqlHTTP({
   schema: graphqlSchema,
   rootValue: graphqlResolver,
@@ -99,3 +121,8 @@ mongoose
     app.listen(8080);
   })
   .catch(err => console.log(err));
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, err => console.log(err));
+};
